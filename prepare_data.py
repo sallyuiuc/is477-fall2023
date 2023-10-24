0547bde832e132d57ae6286da982445865c9bbe6
sallyuiuc <144648104+sallyuiuc@users.noreply.github.com>
2023-10-24 16:50:18 -0500

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,60 +1,41 @@
 import os
 import requests
 import hashlib
-import zipfile
 
 # URLs for the datasets
-adult_zip_url = "https://archive.ics.uci.edu/static/public/2/adult.zip"
-adult_csv_url = "https://raw.githubusercontent.com/socialfoundations/folktables/main/adult_reconstruction.csv"
+adult_data_url=  "https://archive.ics.uci.edu/static/public/2/adult.zip"
+reconstruct_data_url = "https://raw.githubusercontent.com/socialfoundations/folktables/main/adult_reconstruction.csv"
 
-# File paths for downloaded data and files
-data_dir = "data"
-zip_file_path = os.path.join(data_dir, "adult.zip")
-csv_file_path = os.path.join(data_dir, "folktables", "adult_reconstruction.csv")
-zip_hash_file = os.path.join(data_dir, "adult_zip_hash.txt")
-csv_hash_file = os.path.join(data_dir, "adult_csv_hash.txt")
-
-def download_file(url, save_path):
-    try:
-        response = requests.get(url, stream=True)
-        response.raise_for_status()
-
-        with open(save_path, "wb") as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading {url}: {e}")
-        exit(1)
-
-def compute_hash(file_path):
-    sha256_hash = hashlib.sha256()
-    with open(file_path, "rb") as f:
-        for byte_block in iter(lambda: f.read(4096), b""):
-            sha256_hash.update(byte_block)
-    return sha256_hash.hexdigest()
-
-def main():
-    os.makedirs(data_dir, exist_ok=True)
-    # Download the ZIP file
-    if not os.path.isfile(zip_file_path):
-        download_file(adult_zip_url, zip_file_path)
-
-    # Verify the ZIP file's integrity
-    zip_hash = compute_hash(zip_file_path)
-    with open(zip_hash_file, "w") as f:
-        f.write(zip_hash)
-
-    # Unzip the file
-    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
-        zip_ref.extractall(data_dir)
-
-    # Download the CSV file
-    if not os.path.isfile(csv_file_path):
-        download_file(adult_csv_url, csv_file_path)
-
-    # Verify the CSV file's integrity
-    csv_hash = compute_hash(csv_file_path)
-    with open(csv_hash_file, "w") as f:
-        f.write(csv_hash)
-
-    print("Data downloaded and verified successfully.")
+# Check the File paths for downloaded data and files
+Path="data/folktales"
+isExist=os.path.exists(path)
+if not isExist:
+# Create a new directory if the answer is no
+os.makedirs(path)
+response_adult=requests.get(adult_data_url)
+with open('data/adult.zip,more='wb') as f:
+    f.write(response_adult.content)
+filename1='data/adult.zip'
+with open(filename1,mode='rb') as f:
+    data=f.read()
+    sha256hash=hashlib.sha256(data).hexdigest()
+uci_adult_sha256='17ffad1a7f19f0f3ec0548ac595f9d9b9220c75c5e8d042296a202d6886d983d'
+if uci_adult_sha256!=sha256hash:
+    print("not matched")
+else:
+    print("matched")
+    
+# Reconstruced data set
+reconstruct_data_url = "https://raw.githubusercontent.com/socialfoundations/folktables/main/adult_reconstruction.csv"
+response_reconstruct= requests.get(reconstruct_data_url)
+with open('data/folktables/adult_reconstruction.csv',mode='wb') as f:
+    f.write(response_reconstruct.content)
+filename2='data/folktables/adult_reconstruction.csv'
+with open(filename2,mode='rb') as f:
+    data2 = f.read()
+    sha256hash2=hashlib.sha256(data2).hexdigest()
+reconstruct_sha256='215afdcd38c1c91d5d080d48c239318fe45dca3fe5286ceef9495f13a2886b6e'
+if reconstruct_sha256!=sha256hash2:
+    print("not matched")
+else:
+    print("matched")
